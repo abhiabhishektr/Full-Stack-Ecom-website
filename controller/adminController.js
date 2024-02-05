b7652cc5bcb6b9b47b661715a8a9d20a6d2a36b3
abhishek <abhiabhishektr@gmail.com>
2024-02-06 00:35:27 +0530

    waste

--- controller/adminController.js
+++ controller/adminController.js
@@ -260,8 +260,18 @@ const allproducts = async (req, res) => {
 
 const newproducts = async (req, res) => {
     const Category = await categorydb.find();
-    res.render("addProducts", { Category });
+    res.render("newProduct", { Category });
 };
+
+
+// <!-- <input type="file" id="imageCropFileInput" multiple="" accept=".jpg,.jpeg,.png">
+// <input type="hidden" id="profile_img_data">
+// <div class="img-preview"></div>
+// <div id="galleryImages"></div>
+// <div id="cropper">
+//   <canvas id="cropperImg" width="0" height="0"></canvas>
+//   <button class="cropImageBtn" id="cropImageBtn">Crop</button>
+// </div> -->
 
 const category = async (req, res) => {
     let existingCategory = req.query.existingCategory;
